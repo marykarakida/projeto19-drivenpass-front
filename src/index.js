7ada4cca9fab0a9c3415e2fe648ecbf3b7f96212
Mary Karakida <makarakida@gmail.com>
2022-09-10 11:29:20 -0300

    chore: create auth context
    useAuth hook used to access context

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { AuthProvider } from './contexts/AuthProvider';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -8,9 +11,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-            <Routes>
-                <Route path="/*" element={<App />} />
-            </Routes>
+            <AuthProvider>
+                <Routes>
+                    <Route path="/*" element={<App />} />
+                </Routes>
+            </AuthProvider>
         </BrowserRouter>
     </React.StrictMode>
 );
